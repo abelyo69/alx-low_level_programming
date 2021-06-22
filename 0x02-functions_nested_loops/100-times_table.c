34c94e86eaacdeed699d4a301b478d7444d473a9
ubuntu abel <abelyo69@yahoo.com>
2021-06-22 06:48:54 -0700

    102 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,31 +1,47 @@
-#include "holberton.h"
-#include <stdio.h>
+include "holberton.h"
 /**
- * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
- *
- * Return: 0
+ * print_times_table - prints the times table for n.
+ * @n: The multiplication table requested.
+ * Return: Nothing.
  */
-int main(void)
+void print_times_table(int n)
 {
-	int i, j, k;
-	int counter;
+	int i, j, res;
 
-	i = 1;
-	j = 1;
-	k = 0;
-
-	for (counter = 0; counter < 52; counter++)
+	if (!(n > 15 || n < 0))
 	{
-		if (counter == 51)
-			printf("%i\n", i);
-
-		else
+		for (i = 0; i <= n; i++)
 		{
-			printf("%i, ", i);
-			k = i + j;
-			j = i;
-			i = k;
+			for (j = 0; j <= n; j++)
+			{
+				res = (i * j);
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (res < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 10 && res < 100)
+				{
+					_putchar(' ');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 100 && j != 0)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
+				}
+				else
+					_putchar((res % 10) + '0');
+			}
+			_putchar('\n');
 		}
 	}
-	return (0);
 }

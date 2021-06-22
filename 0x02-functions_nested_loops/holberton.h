964a7dead195ba7208b35f9be015b70a0fb72acf
ubuntu abel <abelyo69@yahoo.com>
2021-06-22 04:53:09 -0700

    holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,6 +1,3 @@
-ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -8,10 +5,8 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int);
+int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int n, int m);
 void print_to_98(int n);
-
-#endif /* _HOLBERTON_H_ *

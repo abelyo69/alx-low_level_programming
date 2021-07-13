63b2c802cb4f58a495ba457b0ea615d8fa1c475b
ubuntu abel <abelyo69@yahoo.com>
2021-07-12 23:46:28 -0700

    update

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,39 +1,43 @@
 #include "holberton.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
-  * argstostr - fills memory with a constant byte
-  * @ac: is the size of the pointer
-  * @av: is a letter
-  * Return: an address
-  */
+ * argstostr - prints args
+ * @ac: takes in width of grid
+ * @av: height of grid
+ * Return: the args one line at a time
+ */
 
 char *argstostr(int ac, char **av)
 {
-	char *conca;
-	int i, j, cont = 0, k = 0;
+	char *str;
+	int count = 0, a = 0, b = 0, c = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-
-	for (i = 0; i < ac; i++)
+	while (a < ac)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			cont++;
+		b = 0;
+		while (av[a][b] != '\0')
+		{
+			count++;
+			b++;
+		}
+		a++;
 	}
-
-	conca = malloc(cont * sizeof(char) + ac + 1);
-	if (!conca)
+	count = count + ac + 1;
+	str = malloc(sizeof(char) * count);
+	if (str == NULL)
+	{
 		return (NULL);
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++, k++)
-			conca[k] = av[i][j];
-		conca[k] = '\n';
-		k++;
 	}
-	conca[k] = '\0';
-	return (conca);
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b] != '\0'; b++)
+		{
+			str[c] = av[a][b];
+			c++;
+		}
+		str[c] = '\n';
+		c++;
+	}
+	return (str);
 }
